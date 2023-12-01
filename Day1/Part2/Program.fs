3c904c7c6194dfb12cd2f0fd417a30530b3caabe
ostapsivko <o-stap@hotmail.com>
2023-12-02 01:41:55 +0200

    Advent of Code 2023 - Day 1 pt 1

---
+++ Day1/Part2/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let reductor elem acc = 
+    match acc with
+    | "" -> elem
+    | _ -> acc
+
+let toNum line = 
+    let strings =
+        line
+        |> Seq.filter System.Char.IsDigit
+        |> Seq.map (fun c -> c.ToString())
+
+    match (strings |> List.ofSeq) with
+        | [value] -> value + value |> int
+        | [first; second] -> first + second |> int
+        | head:: tail -> (head + (tail |> List.last)) |> int
+        | _ -> 0
+    //|> Console.WriteLine
+
+let total input = 
+    input
+    |> Array.map toNum
+    |> Array.sum
+
+let lines = File.ReadAllLines("input.txt")
+
+lines |> total |> Console.WriteLine
