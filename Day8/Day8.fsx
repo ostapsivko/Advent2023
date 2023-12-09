94b45422f27a5a3852993029bbcb9b6763541714
ostapsivko <o-stap@hotmail.com>
2023-12-09 11:14:49 +0200

    Day 8 Part 2 OK

--- Day8/Day8.fsx
+++ Day8/Day8.fsx
@@ -33,6 +33,27 @@ module Part1 =
                 let nextNode = nodes |> Seq.find (fun n -> n.Name = node.Right)
                 loopOver (steps + 1) directions nextNode nodes
 
+module Part2 =
+    //courtesy of Rosettacode https://rosettacode.org/wiki/Least_common_multiple
+    let rec gcd (x: int64) (y: int64) = if y = 0 then abs x else gcd y (x % y)
+
+    let lcm x y = x * y / (gcd x y)
+
+    let toLastNode (directions: string) nodes startNode =
+        let mutable steps = 0
+        let mutable node = startNode
+
+        while node.Name.EndsWith('Z') |> not do
+            match directions[steps % directions.Length] with
+            | Left ->
+                node <- nodes |> Seq.find (fun n -> n.Name = node.Left)
+                steps <- steps + 1
+            | Right ->
+                node <- nodes |> Seq.find (fun n -> n.Name = node.Right)
+                steps <- steps + 1
+
+        steps
+
 let toNode (line: string) =
     let parts = line.Split('=', System.StringSplitOptions.TrimEntries)
 
@@ -53,7 +74,15 @@ let leftRight = input[0]
 
 let nodes = input |> Array.skip 2 |> Array.map toNode
 
-let solve =
+let solvePart1 =
     Part1.loopOver 0 leftRight (nodes |> Array.find (fun n -> n.Name = start)) nodes
 
-solve
+let solvePart2 =
+    nodes
+    |> Array.filter (fun n -> n.Name.EndsWith('A'))
+    |> Array.map (Part2.toLastNode leftRight nodes)
+    |> Array.map int64
+    |> Array.reduce (fun x y -> Part2.lcm x y)
+
+solvePart1
+solvePart2
